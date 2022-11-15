c21fcf11c1a368d449b82378b560bd1dbb5f69c2
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-11-15 18:44:04 +0100

    new exercise

---
+++ ex13.py
@@ -0,0 +1,4 @@
+'''Write a Python program to find numbers between 100 and 400 (both included) where each digit of a number is an even number. The numbers obtained should be printed in a comma-separated sequence.'''
+
+for i in range(100, 401, 2):
+    print(i, end=',')
