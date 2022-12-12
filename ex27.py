a408bfce78e576190d2d9c1b7f1b075c80527229
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-12-12 22:19:19 +0100

    new exercise

---
+++ ex27.py
@@ -0,0 +1,63 @@
+"""Functions to keep track and alter inventory."""
+
+
+def create_inventory(items):
+    """Create a dict that tracks the amount (count) of each element on the `items` list.
+
+    :param items: list - list of items to create an inventory from.
+    :return: dict - the inventory dictionary.
+    """
+    l = [items.count(i) for i in items]
+    return dict(zip(items, l))
+
+
+def add_items(inventory, items):
+    """Add or increment items in inventory using elements from the items `list`.
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return: dict - the inventory updated with the new items.
+    """
+    inv = inventory.keys()
+    values = inventory.values()
+    print(list(values))
+    for i in range(list(values)):
+        print(i)
+        items += list(inv)
+
+    return create_inventory(items)
+
+
+print(add_items({"coal": 4}, ["wood", "iron", "coal", "wood"]))
+
+
+def decrement_items(inventory, items):
+    """Decrement items in inventory using elements from the `items` list.
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return: dict - updated inventory with items decremented.
+    """
+
+    pass
+
+
+def remove_item(inventory, item):
+    """Remove item from inventory if it matches `item` string.
+
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return: dict - updated inventory with item removed. Current inventory if item does not match.
+    """
+
+    pass
+
+
+def list_inventory(inventory):
+    """Create a list containing all (item_name, item_count) pairs in inventory.
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+
+    pass
