c151cbb23ecc14a97d6b3b4b87a45cd1d03b5502
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-12-16 15:56:53 +0100

    new exercise

---
+++ ex33.py
@@ -0,0 +1,10 @@
+def leap_year(year):
+    if (year % 400 == 0) and (year % 100 == 0):
+        return True
+    elif (year % 4 == 0) and (year % 100 != 0):
+        return True
+    else:
+        return False
+
+
+print(leap_year(2100))
